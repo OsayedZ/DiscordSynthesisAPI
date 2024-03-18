7e2c75f56a44affcce42cb5a0a5f4450681b3379
OsayedZ <osayedzahida@gmail.com>
2024-03-18 16:37:45 +0200

    added stuff

--- src/main/java/me/osayed/discordsynthesis/core/api/DiscordSynthesisAPI.java
+++ src/main/java/me/osayed/discordsynthesis/core/api/DiscordSynthesisAPI.java
@@ -4,6 +4,15 @@ import me.osayed.discordsynthesis.core.IDSPlayer;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
 public abstract class DiscordSynthesisAPI {
+
+    private static DiscordSynthesisAPI instance;
+
+    public static DiscordSynthesisAPI get() throws Exception {
+        if (instance == null) throw new Exception("Missing implementation");
+        return instance;
+    }
+
+    public abstract IDSPlayer getPlayer(String name);
     public abstract void sendMessage(ChannelType channel, String message);
     public abstract void sendMessage(ChannelType channel, EmbedBuilder builder);
     public abstract void sendChatSyncWebhook(IDSPlayer dsPlayer, String message, String displayName);
